43b153534c4ec97e15c2c9f914c96bf232f57462
filip <filip1folga@gmail.com>
2025-08-25 14:17:43 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -65,7 +65,7 @@ class Order(models.Model):
     )
 
     def __str__(self) -> str:
-        return f"{self.created_at}"
+        return f"Order: {self.created_at}"
 
     class Meta:
         ordering = ["-created_at"]
@@ -87,7 +87,7 @@ class Ticket(models.Model):
 
     def __str__(self) -> str:
 
-        return (f"{self.movie_session.movie.title} "
+        return (f"Ticket: {self.movie_session.movie.title} "
                 f"{self.movie_session.show_time} "
                 f"(row: {self.row}, seat: {self.seat})")
 
@@ -97,15 +97,14 @@ class Ticket(models.Model):
         if (self.seat <= 0
                 or self.seat > self.movie_session.cinema_hall.seats_in_row):
             errors["seat"] = (
-                f"seat number must be in available range:"
-                f" (1, seats_in_row): "
-                f"(1, {self.movie_session.cinema_hall.seats_in_row})"
+                f"For seat: 'seat must be in range"
+                f" 1..{self.movie_session.cinema_hall.seats_in_row}"
             )
 
         if self.row <= 0 or self.row > self.movie_session.cinema_hall.rows:
             errors["row"] = (
-                f"row number must be in available range: (1, rows):"
-                f" (1, {self.movie_session.cinema_hall.rows})"
+                f"For row: 'row must be in range"
+                f" 1..{self.movie_session.cinema_hall.rows}."
             )
 
         if errors:
