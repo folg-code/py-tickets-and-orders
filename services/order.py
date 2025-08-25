8297b04603219c8b4352c5d7b7305f37b3cfe50d
filip <filip1folga@gmail.com>
2025-08-25 13:55:51 +0200

    Solution

--- services/order.py
+++ services/order.py
@@ -9,53 +9,53 @@ from db.models import Order, Ticket, MovieSession
 User = get_user_model()
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: Union[datetime, str] = None,
 ) -> Order:
-    with transaction.atomic():
-        user = User.objects.get(username=username)
+    user = User.objects.get(username=username)
 
-        if isinstance(date, str):
-            try:
-                created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
-            except ValueError:
-                raise ValueError(
-                    f"Invalid date format: {date}. Expected '%Y-%m-%d %H:%M'."
-                )
-        elif isinstance(date, datetime):
-            created_at = date
-        elif date is None:
-            created_at = None
-        else:
-            raise TypeError(
-                f"Date must be str in format "
-                f"'%Y-%m-%d %H:%M' or datetime, not {type(date)}"
+    if isinstance(date, str):
+        try:
+            created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+        except ValueError:
+            raise ValueError(
+                f"Invalid date format: {date}. Expected '%Y-%m-%d %H:%M'."
             )
-        order_data = {"user": user}
-        if created_at is not None:
-            order_data["created_at"] = created_at
+    elif isinstance(date, datetime):
+        created_at = date
+    elif date is None:
+        created_at = None
+    else:
+        raise TypeError(
+            f"Date must be str in format "
+            f"'%Y-%m-%d %H:%M' or datetime, not {type(date)}"
+        )
+    order_data = {"user": user}
+    if created_at is not None:
+        order_data["created_at"] = created_at
 
-        order = Order.objects.create(**order_data)
-        if created_at:
-            order.created_at = created_at
-            order.save(update_fields=["created_at"])
+    order = Order.objects.create(**order_data)
+    if created_at:
+        order.created_at = created_at
+        order.save(update_fields=["created_at"])
 
-        ticket_objs = []
-        for ticket in tickets:
-            movie_session = (MovieSession.objects
-                             .get(id=ticket["movie_session"]))
-            ticket_objs.append(
-                Ticket(
-                    order=order,
-                    row=ticket["row"],
-                    seat=ticket["seat"],
-                    movie_session=movie_session
-                )
+    ticket_objs = []
+    for ticket in tickets:
+        movie_session = (MovieSession.objects
+                         .get(id=ticket["movie_session"]))
+        ticket_objs.append(
+            Ticket(
+                order=order,
+                row=ticket["row"],
+                seat=ticket["seat"],
+                movie_session=movie_session
             )
-        Ticket.objects.bulk_create(ticket_objs)
-        return order
+        )
+    Ticket.objects.bulk_create(ticket_objs)
+    return order
 
 
 def get_orders(username: str = None) -> QuerySet[Order]:
